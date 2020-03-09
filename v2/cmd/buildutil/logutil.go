8a27de7eb7cdb22f16803c17650d532b4d127bb1
Philipp Trulson <philipp@trulson.de>
2020-03-09 14:50:32 +0100

    Only print colored output on terminals

--- v2/cmd/buildutil/logutil.go
+++ v2/cmd/buildutil/logutil.go
@@ -7,12 +7,16 @@ import (
 
 	"github.com/rebuy-de/rebuy-go-sdk/v2/pkg/cmdutil"
 	"github.com/tidwall/pretty"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func dumpJSON(data interface{}) {
 	b, err := json.MarshalIndent(data, "", "    ")
 	cmdutil.Must(err)
 
-	b = pretty.Color(b, pretty.TerminalStyle)
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		b = pretty.Color(b, pretty.TerminalStyle)
+	}
+
 	fmt.Fprintln(os.Stderr, string(b))
 }
--- v2/go.mod
+++ v2/go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 	github.com/tidwall/pretty v1.0.0
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
 	golang.org/x/tools v0.0.0-20200203023011-6f24f261dadb
 	gopkg.in/yaml.v2 v2.2.8
 	k8s.io/client-go v0.17.2
