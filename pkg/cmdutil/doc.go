6295cc975d06947c8ad4fab0cc4720b200cd4bd9
Sven Walter <s.walter@rebuy.com>
2024-06-11 14:27:56 +0200

    apply go fmt

--- pkg/cmdutil/doc.go
+++ pkg/cmdutil/doc.go
@@ -4,7 +4,7 @@
 // An example application can be found at
 // https://github.com/rebuy-de/golang-template.
 //
-// Graceful Application Exits
+// # Graceful Application Exits
 //
 // In many command line applications it is desired to exit the process
 // immediately, if it is clear that the application cannot recover. Important
@@ -25,19 +25,19 @@
 // struct and catches it right before the application exit. This is an example
 // to illustrate the usage:
 //
-//   func main() {
-//     defer cmdutil.HandleExit()
-//     run()
-//   }
+//	func main() {
+//	  defer cmdutil.HandleExit()
+//	  run()
+//	}
 //
-//   func run() {
-//     defer fmt.Println("important cleanup")
-//     err := doSomething()
-//     if err != nil {
-//       log.Error(err)
-//       cmdutil.Exit(2)
-//     }
-//   }
+//	func run() {
+//	  defer fmt.Println("important cleanup")
+//	  err := doSomething()
+//	  if err != nil {
+//	    log.Error(err)
+//	    cmdutil.Exit(2)
+//	  }
+//	}
 //
 // The defer of HandleExit is the first statement in the main function. It
 // ensures a pretty output and that the application exits with the specified
@@ -47,7 +47,7 @@
 // error actually appears on external logging applications like Syslog or
 // Graylog.
 //
-// Minimal Application Boilerplate
+// # Minimal Application Boilerplate
 //
 // Golang is very helpful for creating glue code in the ops area and creating
 // micro services. But when you want features like proper logging, a version
@@ -55,25 +55,25 @@
 // needed. NewRootCommand creates a ready-to-use Cobra command to reduce the
 // necessary code. This is an example to illustrate the usage:
 //
-//   type App struct {
-//       Name string
-//   }
+//	type App struct {
+//	    Name string
+//	}
 //
-//   func (app *App) Run(cmd *cobra.Command, args []string) {
-//       log.Infof("hello %s", app.Name)
-//   }
+//	func (app *App) Run(cmd *cobra.Command, args []string) {
+//	    log.Infof("hello %s", app.Name)
+//	}
 //
-//   func (app *App) Bind(cmd *cobra.Command) {
-//       cmd.PersistentFlags().StringVarP(
-//           &app.Name, "name", "n", "world",
-//           `Your name.`)
-//   }
+//	func (app *App) Bind(cmd *cobra.Command) {
+//	    cmd.PersistentFlags().StringVarP(
+//	        &app.Name, "name", "n", "world",
+//	        `Your name.`)
+//	}
 //
-//   func NewRootCommand() *cobra.Command {
-//       cmd := cmdutil.NewRootCommand(new(App))
-//       cmd.Short = "an example app for golang which can be used as template"
-//       return cmd
-//   }
+//	func NewRootCommand() *cobra.Command {
+//	    cmd := cmdutil.NewRootCommand(new(App))
+//	    cmd.Short = "an example app for golang which can be used as template"
+//	    return cmd
+//	}
 //
 // The App struct contains fields for parameters which are defined in Bind or
 // for internal states which might get defined while running the application.
@@ -82,12 +82,12 @@
 // the compiled version of the application and other build parameters. These
 // values need to be set by the build system via ldflags.
 //
-//   BUILD_XDST=$(pwd)/vendor/github.com/rebuy-de/rebuy-go-sdk/cmdutil
-//   go build -ldflags "\
-//     -X '${BUILD_XDST}.BuildName=${NAME}' \
-//     -X '${BUILD_XDST}.BuildPackage=${PACKAGE}' \
-//     -X '${BUILD_XDST}.BuildVersion=${BUILD_VERSION}' \
-//     -X '${BUILD_XDST}.BuildDate=${BUILD_DATE}' \
-//     -X '${BUILD_XDST}.BuildHash=${BUILD_HASH}' \
-//     -X '${BUILD_XDST}.BuildEnvironment=${BUILD_ENVIRONMENT}' \
+//	BUILD_XDST=$(pwd)/vendor/github.com/rebuy-de/rebuy-go-sdk/cmdutil
+//	go build -ldflags "\
+//	  -X '${BUILD_XDST}.BuildName=${NAME}' \
+//	  -X '${BUILD_XDST}.BuildPackage=${PACKAGE}' \
+//	  -X '${BUILD_XDST}.BuildVersion=${BUILD_VERSION}' \
+//	  -X '${BUILD_XDST}.BuildDate=${BUILD_DATE}' \
+//	  -X '${BUILD_XDST}.BuildHash=${BUILD_HASH}' \
+//	  -X '${BUILD_XDST}.BuildEnvironment=${BUILD_ENVIRONMENT}' \
 package cmdutil
